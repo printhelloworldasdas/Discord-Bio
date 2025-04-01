a7d8556122f2c339005cd2ae03938a515da57869
printhelloworldasdas <165765404+printhelloworldasdas@users.noreply.github.com>
2025-04-01 17:13:53 +0200

    Create Visitors.js

---
+++ server/models/Visitors.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const visitorSchema = new mongoose.Schema({
+    count: { type: Number, default: 0 },
+    lastUpdated: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Visitor', visitorSchema);
