add2de366d2eee44827a3e1fb52e3b3cbe82c47b
printhelloworldasdas <165765404+printhelloworldasdas@users.noreply.github.com>
2025-04-01 17:16:02 +0200

    Create auth.js

---
+++ server/middleware/auth.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+    if (req.session && req.session.admin) {
+        return next();
+    }
+    res.status(401).json({ success: false, message: 'Unauthorized' });
+};
